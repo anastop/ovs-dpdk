8a8689a353d889d312e73b9bfda6a03d195e701d
Brian Eiler <brianeiler@gmail.com>
2019-04-02 00:39:07 -0400

    Fixed the script paths

--- scripts/build-script-3.sh
+++ scripts/build-script-3.sh
@@ -5,21 +5,21 @@ echo
 echo "Installing DPDK 17.11.4..."
 tar xvf dpdk-17.11.4.tar.xz
 ln -rs -v dpdk-stable-17.11.4 /root/ovs-dpdk/dpdk
-./compile_dpdk.sh 
+/root/ovs-dpdk/scripts/compile_dpdk.sh 
 echo
 echo "Done"
 echo
 echo "Installing OVS-2.10.1..."
 tar xvf openvswitch-2.10.1.tar.gz
 ln -rs -v openvswitch-2.10.1 /root/ovs-dpdk/ovs
-./compile_ovs.sh 
+/root/ovs-dpdk/scripts/compile_ovs.sh 
 echo
 echo "Done"
 echo
 echo " Installing qemu-2.12.1..."
 tar xvf qemu-2.12.1.tar.xz
 ln -rs -v qemu-2.12.1 /root/ovs-dpdk/qemu
-./compile_qemu.sh
+/root/ovs-dpdk/scripts/compile_qemu.sh
 echo
 echo "Done"
 echo
