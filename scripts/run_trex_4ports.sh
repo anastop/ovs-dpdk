ec0a97433e67b28d67e85f35dded018048d33780
Brian Eiler <brianeiler@gmail.com>
2019-04-02 03:17:45 -0400

    trex config

--- scripts/run_trex_4ports.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-cd /root/
-echo "Starting Trex server"
-cd /root/ovs-dpdk/trex/
-./t-rex-64 -i  --cfg /etc/trex_cfg-6port.yaml
---
+++ scripts/trex_server_4ports.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+cd /root/ovs-dpdk
+cp ./configs/trex_cfg-4ports.yaml /etc
+echo "Starting Trex server"
+cd /root/ovs-dpdk/trex/
+./t-rex-64 -i  --cfg /etc/trex_cfg-4ports.yaml
+
