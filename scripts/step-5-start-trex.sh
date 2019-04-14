1878f5376924560ee5a7289444a73fdcaeae7af3
Brian Eiler <brianeiler@gmail.com>
2019-04-14 00:11:18 -0400

    Update step-5-start-trex.sh

--- scripts/step-5-start-trex.sh
+++ scripts/step-5-start-trex.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-cp /opt/ovs-dpdk-lab/configs/trex/trex_cfg /etc
+cp /opt/ovs-dpdk-lab/configs/trex/trex_cfg.yaml /etc
 echo 8192 > /sys/devices/system/node/node0/hugepages/hugepages-2048kB/nr_hugepages
 umount /mnt/huge
 mount -t hugetlbfs nodev /mnt/huge -o pagesize=2M
