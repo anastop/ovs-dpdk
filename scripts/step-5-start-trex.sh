d5f01380edb4a17163b3d534fc083c5d0c04ef20
Brian Eiler <brianeiler@gmail.com>
2019-04-14 17:09:27 -0400

    Converted to file mode

--- scripts/step-5-start-trex.sh
+++ scripts/step-5-start-trex.sh
@@ -3,4 +3,6 @@
 cp /opt/ovs-dpdk-lab/configs/trex/trex_cfg.yaml /etc
 echo "Starting Trex server"
 cd /opt/ovs-dpdk-lab/trex/
-./t-rex-64 -i --cfg /etc/trex_cfg.yaml
+./t-rex-64 -f /etc/trex_cfg.yaml
+
+#./t-rex-64 -i --cfg /etc/trex_cfg.yaml
