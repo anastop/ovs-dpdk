3dd8fc3040e3b26b74619737c578382c74758ee5
Brian Eiler <brianeiler@gmail.com>
2019-04-15 17:10:37 -0400

    Update compile_qemu.sh

--- source/compile_qemu.sh
+++ source/compile_qemu.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 apt install -y libglib2.0-dev libfdt-dev libpixman-1-dev zlib1g-dev
+apt install -y libattr1 libattr1-dev libcap-dev libcap-ng-dev
+apt install -y linux-image-extra-virtual
 
 cd /opt/ovs-dpdk-lab/qemu/
 ./configure --target-list=x86_64-softmmu --enable-virtfs
