ed2f659d8db295149d27b8663e0fb76d88a9a4f0
Brian Eiler <brianeiler@gmail.com>
2019-04-16 00:34:58 -0400

    Update step-3-start-ovs.sh

--- scripts/step-3-start-ovs.sh
+++ scripts/step-3-start-ovs.sh
@@ -43,7 +43,7 @@ $OVS_DIR/utilities/ovs-vsctl --no-wait init
 # Configure OVS with the CPU cores and RAM for the SECOND NUMA node
 $OVS_DIR/utilities/ovs-vsctl --no-wait set Open_vSwitch . other_config:dpdk-init=true 
 $OVS_DIR/utilities/ovs-vsctl --no-wait set Open_vSwitch . other_config:dpdk-lcore-mask=0x200000  	# This is core 21, the second core in the second NUMA node
-$OVS_DIR/utilities/ovs-vsctl --no-wait set Open_vSwitch . other_config:dpdk-socket-mem="0,2048"		# This assigns 2GB of RAM to the second NUMA node, and none to the first
+$OVS_DIR/utilities/ovs-vsctl --no-wait set Open_vSwitch . other_config:dpdk-socket-mem="0,2048" 	# This assigns 2GB of RAM to the second NUMA node, and none to the first
 $OVS_DIR/vswitchd/ovs-vswitchd unix:$DB_SOCK \
 	--pidfile \
 	--detach  \
