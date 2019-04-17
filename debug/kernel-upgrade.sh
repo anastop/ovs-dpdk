201c0d2ccd9dec78e4297b0ae4454a3cdd27856a
Brian Eiler <brianeiler@gmail.com>
2019-04-17 14:24:22 -0400

    Create kernel-upgrade.sh

---
+++ debug/kernel-upgrade.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd /tmp/
+wget -c https://kernel.ubuntu.com/~kernel-ppa/mainline/v4.20/linux-headers-4.20.0-042000_4.20.0-042000.201812232030_all.deb
+wget -c https://kernel.ubuntu.com/~kernel-ppa/mainline/v4.20/linux-headers-4.20.0-042000-generic_4.20.0-042000.201812232030_amd64.deb
+wget -c https://kernel.ubuntu.com/~kernel-ppa/mainline/v4.20/linux-image-unsigned-4.20.0-042000-generic_4.20.0-042000.201812232030_amd64.deb
+wget -c https://kernel.ubuntu.com/~kernel-ppa/mainline/v4.20/linux-modules-4.20.0-042000-generic_4.20.0-042000.201812232030_amd64.deb
+dpkg -i *.deb
+update-grub
+init 6
