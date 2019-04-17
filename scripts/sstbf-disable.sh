2780911ea45a7da2187f3451485107b158874a8a
Brian Eiler <brianeiler@gmail.com>
2019-04-17 14:54:02 -0400

    added scripts to enable/disable SSTBF

---
+++ scripts/sstbf-disable.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd /opt/ovs-dpdk-lab/debug/
+./clx_baseline.sh
+
+echo
+echo "SST-BF is DISABLED. Processors are back to their defaults."
+echo
---
+++ scripts/sstbf-enable.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+/opt/ovs-dpdk-lab/debug/pbf.py -s
+
+echo
+echo "SST-BF is ACTIVE."
+echo
