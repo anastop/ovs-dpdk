00f34bdf6756cd043a5dee7492f521b91b9e072a
Brian Eiler <brianeiler@gmail.com>
2019-04-18 01:00:59 -0400

    Added Global Variable source

--- configs/global-variables.sh
+++ configs/global-variables.sh
@@ -1,6 +1,13 @@
+#!/bin/bash
 #
+# This is a list of environment variables that should be included in scripts by using the bash "source" command.
+# These are intended to be global variables across all the OVS-DPDK related scripts.
+#
+
+
+
 # High Power CPU Mapping - 0=Low 1=High
-#
+# -------------------------------------
 # Note this reads RIGHT to LEFT
 # This is for creating the CPU masks for OVS
 # Copy the bits into a binary to hex converter
@@ -16,8 +23,8 @@ cpu_numa2_high=(x x x)
 cpu_numa2_low=(y y y)
 
 # These are the global variables that contain the core number you want used by that service
-cpu_ovs_lcpu_mask=
-cpu_ovs_pmd_mask=
+cpu_ovs_lcpu_mask="0x200000"
+cpu_ovs_pmd_mask="0xCC00000000CC000000"   #4C8T -- uses CPUs 26,27,30,31,66,67,70,71
 cpu_ovs_dpdk0=
 cpu_ovs_dpdk1=
 cpu_ovs_dpdk2=
