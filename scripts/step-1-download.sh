f2e70b42f37deac5e7f4f843267664652f164a87
Brian Eiler <brianeiler@gmail.com>
2019-04-18 12:33:10 -0400

    Replaced with public URLs

--- scripts/step-1-download.sh
+++ scripts/step-1-download.sh
@@ -7,10 +7,10 @@ source /etc/0-ovs-dpdk-global-variables.sh
 cd ${git_base_path}/source
 echo
 echo "Downloading source packages..."
-wget https://www.dropbox.com/s/rby2eoaj07zoofd/trex-v2.53.tgz
-wget https://www.dropbox.com/s/5y39v3hrgnk43b4/openvswitch-2.11.0.tar.gz
-wget https://www.dropbox.com/s/bau0d0gxbo670qm/qemu-3.1.0.tar.xz
-wget https://www.dropbox.com/s/ozk0i3baq7zxau9/dpdk-18.11.1.tar.xz
+wget https://trex-tgn.cisco.com/trex/release/v2.56.tar.gz
+wget https://www.openvswitch.org/releases/openvswitch-2.11.0.tar.gz
+wget https://download.qemu.org/qemu-3.1.0.tar.xz
+wget https://fast.dpdk.org/rel/dpdk-18.11.1.tar.xz
 
 echo
 echo "Done Downloading source packages."
