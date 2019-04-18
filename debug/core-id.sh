7ad5d1a48df407fe82691bce7e18c5e1f78f3d1f
Brian Eiler <brianeiler@gmail.com>
2019-04-18 19:40:35 -0400

    Update core-id.sh

--- debug/core-id.sh
+++ debug/core-id.sh
@@ -7,8 +7,7 @@ CPURANGE=$[$(cat /proc/cpuinfo | grep processor | wc -l)-1]
 
 for i in $(seq 0 ${CPURANGE})
 do
-   cat ${CPUPATH}/cpu${i}/cpufreq/base_frequency 
-   # > CORE_BASE_FREQ[${i}]
+   CORE_BASE_FREQ[${i}]=$(( cat ${CPUPATH}/cpu${i}/cpufreq/base_frequency ))
 done
 
 echo "core 4 = " ${CORE_BASE_FREQ[4]}
