dde78bd42e8f9e0e837d743de704b0f016f8381e
Brian Eiler <brianeiler@gmail.com>
2019-04-18 17:58:58 -0400

    Update 2-package_download.sh

--- pre-scripts/2-package_download.sh
+++ pre-scripts/2-package_download.sh
@@ -2,6 +2,8 @@
 
 # Copy the global variable file into place
 cp 0-ovs-dpdk-global-variables.sh /etc
+echo "source /etc/0-ovs-dpdk-global-variables.sh" >> /etc/bash.bashrc
+
 
 apt update
 apt install -y curl
