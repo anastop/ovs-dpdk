9f9435361f3d22695b9227f7e6c568714bb25473
Brian Eiler <brianeiler@gmail.com>
2019-04-22 15:55:14 -0400

    Update 1-kernel_upgrade.sh

--- pre-scripts/1-kernel_upgrade.sh
+++ pre-scripts/1-kernel_upgrade.sh
@@ -7,4 +7,10 @@ wget -c https://kernel.ubuntu.com/~kernel-ppa/mainline/v4.20/linux-image-unsigne
 wget -c https://kernel.ubuntu.com/~kernel-ppa/mainline/v4.20/linux-modules-4.20.0-042000-generic_4.20.0-042000.201812232030_amd64.deb
 dpkg -i *.deb
 update-grub
+
+modprobe -rv ipmi_si
+modprobe -rv ipmi_devintf
+systemctl stop openipmi
+systemctl start openipmi
+
 init 6
