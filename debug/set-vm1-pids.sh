51b03b4be6485d0d7c0a0d30eaa66e5c42994ec1
Brian Eiler <brianeiler@gmail.com>
2019-04-24 10:07:03 -0700

    update

--- debug/set-vm1-pids.sh
+++ debug/set-vm1-pids.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 source /etc/0-ovs-dpdk-global-variables.sh
 
+echo Original...
 taskset -pc -a $1
+echo
 taskset -pc -a ${cpu_vm1_core0},${cpu_vm1_core1} $1
+echo 
 taskset -pc ${cpu_vm1_core2} $2
+echo
 taskset -pc ${cpu_vm1_core3} $3
+echo
+echo Now...
 taskset -pc -a $1
--- debug/set-vm2-pids.sh
+++ debug/set-vm2-pids.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 source /etc/0-ovs-dpdk-global-variables.sh
 
+echo Original...
 taskset -pc -a $1
+echo
 taskset -pc -a ${cpu_vm2_core0},${cpu_vm2_core1} $1
+echo 
 taskset -pc ${cpu_vm2_core2} $2
+echo
 taskset -pc ${cpu_vm2_core3} $3
+echo
+echo Now...
 taskset -pc -a $1
