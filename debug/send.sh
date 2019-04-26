d6bfe42f97d5ce43a22d92d9c2d270c56a4e0860
Brian Eiler <brianeiler@gmail.com>
2019-04-26 17:16:07 -0400

    Update send.sh

--- debug/send.sh
+++ debug/send.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 source /etc/0-ovs-dpdk-global-variables.sh
-curl -v --upload-file $1 https://storage.googleapis.com/incoming-lab/images/$1
+
+source_name=${1}
+target_name=`hostname`-${1}
+target_path="https://storage.googleapis.com/incoming-lab"
+echo
+echo "source name: "${source_name}
+echo "target name: "${target_name}
+echo "target path: "${target_path}
+echo
+
+# curl -v --upload-file ${source_name} ${target_path}/${target_name}
