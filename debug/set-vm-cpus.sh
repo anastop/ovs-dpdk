6301b1eba66746f20c39cd109dbf857c12edca8e
Brian Eiler <brianeiler@gmail.com>
2019-04-29 14:25:27 -0400

    Update set-vm-cpus.sh

--- debug/set-vm-cpus.sh
+++ debug/set-vm-cpus.sh
@@ -1,48 +1,43 @@
 #!/bin/bash
 source /etc/0-ovs-dpdk-global-variables.sh
 
+if [ "$#" -ne 5 ]; then
+    echo
+    echo "You must enter exactly 5 command line arguments"
+    echo
+    echo "Usage: 'cpu-set.sh <VM_NAME> <Core_0> <Core_1> <Core_2> <Core_3>'"
+    echo
+    echo "Where:"
+    echo "  <VM_NAME> is the name of the virtual machine. The text following -name in the QEMU command."
+    echo "  <Core_#> is the CPU core number (of the host) to be assigned to the VM CPU #."
+    echo
+    echo "Example: 'cpu-set.sh VPP-VM1 23 24 25 26'"
+    echo
+fi
+
+vm_name=$1
+vm_cpu_core0=$2
+vm_cpu_core1=$3
+vm_cpu_core2=$4
+vm_cpu_core3=$5
+
 echo
-echo "Setting CPU affinity for VPP-VM1..."
+echo "Setting CPU affinity for ${vm_name}..."
 echo
-vm_1_pid=`ps awx | grep VPP-VM1 | grep -v grep | awk '{print $1}'`
-vm_1_cpu0_pid=`top -b -n 1 -H -p ${vm_1_pid} | grep "CPU 0" | awk '{print $1}'`
-vm_1_cpu1_pid=`top -b -n 1 -H -p ${vm_1_pid} | grep "CPU 1" | awk '{print $1}'`
-vm_1_cpu2_pid=`top -b -n 1 -H -p ${vm_1_pid} | grep "CPU 2" | awk '{print $1}'`
-vm_1_cpu3_pid=`top -b -n 1 -H -p ${vm_1_pid} | grep "CPU 3" | awk '{print $1}'`
-taskset -pc -a ${cpu_vm1_core0} ${vm_1_pid} > /dev/null 2>&1
-taskset -pc ${cpu_vm1_core0} ${vm_1_cpu0_pid} > /dev/null 2>&1
-taskset -pc ${cpu_vm1_core1} ${vm_1_cpu1_pid} > /dev/null 2>&1
-taskset -pc ${cpu_vm1_core2} ${vm_1_cpu2_pid} > /dev/null 2>&1
-taskset -pc ${cpu_vm1_core3} ${vm_1_cpu3_pid} > /dev/null 2>&1
-echo "VPP-VM1 Parent Process" $(taskset -pc ${vm_1_pid} | awk '{$1=$2=""; print $0}')
+vm_pid=`ps awx | grep ${vm_name} | grep -v grep | awk '{print $1}'`
+vm_cpu0_pid=`top -b -n 1 -H -p ${vm_pid} | grep "CPU 0" | awk '{print $1}'`
+vm_cpu1_pid=`top -b -n 1 -H -p ${vm_pid} | grep "CPU 1" | awk '{print $1}'`
+vm_cpu2_pid=`top -b -n 1 -H -p ${vm_pid} | grep "CPU 2" | awk '{print $1}'`
+vm_cpu3_pid=`top -b -n 1 -H -p ${vm_pid} | grep "CPU 3" | awk '{print $1}'`
+taskset -pc -a ${vm_cpu_core0} ${vm_pid} > /dev/null 2>&1
+taskset -pc ${vm_cpu_core0} ${vm_cpu0_pid} > /dev/null 2>&1
+taskset -pc ${vm_cpu_core1} ${vm_cpu1_pid} > /dev/null 2>&1
+taskset -pc ${vm_cpu_core2} ${vm_cpu2_pid} > /dev/null 2>&1
+taskset -pc ${vm_cpu_core3} ${vm_cpu3_pid} > /dev/null 2>&1
+echo "${vm_name} Parent Process" $(taskset -pc ${vm_pid} | awk '{$1=$2=""; print $0}')
 echo "------------------------------------------------"
-echo "VPP-VM1 KVM CPU Core 0" $(taskset -pc ${vm_1_cpu0_pid} | awk '{$1=$2=""; print $0}')
-echo "VPP-VM1 KVM CPU Core 1" $(taskset -pc ${vm_1_cpu1_pid} | awk '{$1=$2=""; print $0}')
-echo "VPP-VM1 KVM CPU Core 2" $(taskset -pc ${vm_1_cpu2_pid} | awk '{$1=$2=""; print $0}')
-echo "VPP-VM1 KVM CPU Core 3" $(taskset -pc ${vm_1_cpu3_pid} | awk '{$1=$2=""; print $0}')
-echo
-echo
-echo
-echo
-echo "Setting CPU affinity for VPP-VM2..."
-echo
-vm_2_pid=`ps awx | grep VPP-VM2 | grep -v grep | awk '{print $1}'`
-vm_2_cpu0_pid=`top -b -n 1 -H -p ${vm_2_pid} | grep "CPU 0" | awk '{print $1}'`
-vm_2_cpu1_pid=`top -b -n 1 -H -p ${vm_2_pid} | grep "CPU 1" | awk '{print $1}'`
-vm_2_cpu2_pid=`top -b -n 1 -H -p ${vm_2_pid} | grep "CPU 2" | awk '{print $1}'`
-vm_2_cpu3_pid=`top -b -n 1 -H -p ${vm_2_pid} | grep "CPU 3" | awk '{print $1}'`
-taskset -pc -a ${cpu_vm2_core0} ${vm_2_pid} > /dev/null 2>&1
-taskset -pc ${cpu_vm2_core0} ${vm_2_cpu0_pid} > /dev/null 2>&1
-taskset -pc ${cpu_vm2_core1} ${vm_2_cpu1_pid} > /dev/null 2>&1
-taskset -pc ${cpu_vm2_core2} ${vm_2_cpu2_pid} > /dev/null 2>&1
-taskset -pc ${cpu_vm2_core3} ${vm_2_cpu3_pid} > /dev/null 2>&1
-echo "VPP-VM2 Parent Process" $(taskset -pc ${vm_2_pid} | awk '{$1=$2=""; print $0}')
-echo "------------------------------------------------"
-echo "VPP-VM2 KVM CPU Core 0" $(taskset -pc ${vm_2_cpu0_pid} | awk '{$1=$2=""; print $0}')
-echo "VPP-VM2 KVM CPU Core 1" $(taskset -pc ${vm_2_cpu1_pid} | awk '{$1=$2=""; print $0}')
-echo "VPP-VM2 KVM CPU Core 2" $(taskset -pc ${vm_2_cpu2_pid} | awk '{$1=$2=""; print $0}')
-echo "VPP-VM2 KVM CPU Core 3" $(taskset -pc ${vm_2_cpu3_pid} | awk '{$1=$2=""; print $0}')
-echo
-echo
-echo "Done settings VM CPU Affinity."
+echo "${vm_name} KVM CPU Core 0" $(taskset -pc ${vm_cpu0_pid} | awk '{$1=$2=""; print $0}')
+echo "${vm_name} KVM CPU Core 1" $(taskset -pc ${vm_cpu1_pid} | awk '{$1=$2=""; print $0}')
+echo "${vm_name} KVM CPU Core 2" $(taskset -pc ${vm_cpu2_pid} | awk '{$1=$2=""; print $0}')
+echo "${vm_name} KVM CPU Core 3" $(taskset -pc ${vm_cpu3_pid} | awk '{$1=$2=""; print $0}')
 echo
