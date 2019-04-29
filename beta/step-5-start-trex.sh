47c11921c0e6c4a8f44eda4636fca2d758f86704
Brian Eiler <brianeiler@gmail.com>
2019-04-29 18:09:03 -0400

    Update step-5-start-trex.sh

--- beta/step-5-start-trex.sh
+++ beta/step-5-start-trex.sh
@@ -56,35 +56,15 @@ fi
 cd ${trex_dir}
 ./trex_daemon_server start
 
-# 
-# 
-# cd ${trex_dir}
-# trex_server_cmd="./t-rex-64 -i --cfg ${yaml_file} --iom 0 -v 4"
-# 
-# rm -fv /tmp/trex.server.out
-# screen -dmS trex -t server ${trex_server_cmd}
-# screen -x trex -X chdir /tmp
-# screen -x trex -p server -X logfile trex.server.out
-# screen -x trex -p server -X logtstamp on
-# screen -x trex -p server -X log on
-# 
-# echo
-# echo "Waiting for the TRex server to be ready..."
-# echo
-# echo "Please be patient, this may take up to 2 minutes."
-# echo
-# echo "netstat -tln | grep -E :4500\|:4501"
-# 
-# echo "Session: screen -x trex"
-# echo "Logs:    cat /tmp/trex.server.out"
-# echo
-# echo "Done. The TRex server is online"
-# echo
-# 
 
 echo
-echo "To launch config: " cd ${trex_dir}; ${trex_dir}/trex-console --batch ${git_base_path}/beta/trex-init-script.conf
-echo "To launch load: " ${git_base_path}/beta/trex-load-64byte-base.sh
+echo "Step 1: Launch config:"
+echo "        cd ${trex_dir}"
+echo "        ${trex_dir}/trex-console --batch ${git_base_path}/beta/trex-init-script.conf"
 echo
-echo "To launch the TRex Console, type: ${trex_dir}/trex-console -f"
+echo "Step 2: Launch streams:"
+echo "        ${git_base_path}/beta/trex-load-64byte-base.sh"
+echo
+echo "Step 3: Launch the TRex Console:"
+echo "        ${git_base_path}/beta/trex-console.sh"
 echo 
