48a8610cfd987046a5b676a28ad46069a9281542
Brian Eiler <brianeiler@gmail.com>
2019-04-29 17:15:35 -0400

    Update cpu-set.sh

--- debug/cpu-set.sh
+++ debug/cpu-set.sh
@@ -34,8 +34,8 @@ else
 	echo
 	echo "Setting CPU affinity for ${vm_name}..."
 	echo
-	ps awx | grep ${vm_name} | grep -v grep | awk '{print $1}' > ~./tmp-pid-list
-	vm_pid=$(cat ~./tmp-pid-list)
+	ps awx | grep ${vm_name} | grep -v grep | awk '{print $1}' > ~/tmp-pid-list
+	vm_pid=$(cat ~/tmp-pid-list)
 	echo 
 	echo "vm_pid = "${vm_pid}
 	echo
