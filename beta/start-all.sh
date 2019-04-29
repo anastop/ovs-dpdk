90e09ca10874765093d937f3928da383106514db
Brian Eiler <brianeiler@gmail.com>
2019-04-29 14:51:16 -0400

    Update start-all.sh

--- beta/start-all.sh
+++ beta/start-all.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Load the custom global environment variables
+source /etc/0-ovs-dpdk-global-variables.sh
+
+
 ${git_base_path}/scripts/step-3-start-ovs.sh
 ${git_base_path}/scripts/step-4-start-vms.sh
 ${git_base_path}/beta/step-5-start-trex.sh
