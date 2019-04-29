04edb6ec38e677237996f0e70ad7e8ba37916e4d
Brian Eiler <brianeiler@gmail.com>
2019-04-29 12:24:55 -0400

    Update set-vm2-pids.sh

--- debug/set-vm2-pids.sh
+++ debug/set-vm2-pids.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 source /etc/0-ovs-dpdk-global-variables.sh
 
-echo Original...
-taskset -pc -a $1
-echo
+# echo Original...
+# taskset -pc -a $1
+# echo
+# taskset -pc -a ${cpu_vm2_core0},${cpu_vm2_core1} $1
+# echo 
+# taskset -pc ${cpu_vm2_core2} $2
+# echo
+# taskset -pc ${cpu_vm2_core3} $3
+# echo
+# echo Now...
+# taskset -pc -a $1
+
 taskset -pc -a ${cpu_vm2_core0},${cpu_vm2_core1} $1
-echo 
-taskset -pc ${cpu_vm2_core2} $2
-echo
-taskset -pc ${cpu_vm2_core3} $3
-echo
-echo Now...
+qemu-affinity -k 2:${cpu_vm2_core2} 3:${cpu_vm2_core3} -- $1
 taskset -pc -a $1
