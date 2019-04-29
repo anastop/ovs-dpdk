da2c6b1264cdadf2b9771895b7e523119b51b60e
Brian Eiler <brianeiler@gmail.com>
2019-04-29 13:04:58 -0400

    Update set-vm-cpus.sh

--- debug/set-vm-cpus.sh
+++ debug/set-vm-cpus.sh
@@ -24,11 +24,15 @@ echo "Setting CPU affinity for VPP-VM2."
 # taskset -pc -a ${cpu_vm2_core0},${cpu_vm2_core1} ${vm_2_pid}
 # qemu-affinity -k 2:${cpu_vm2_core2} 3:${cpu_vm2_core3} -- ${vm_2_pid}
 # taskset -pc -a ${vm_2_pid}
-vm_2_pid=`ps awx | grep VPP-VM1 | grep -v grep | awk '{print $1}'`
+vm_2_pid=`ps awx | grep VPP-VM2 | grep -v grep | awk '{print $1}'`
 vm_2_cpu0_pid=`top -n 1 -H -p ${vm_2_pid} | grep "CPU 0" | awk '{print $1}'`
 vm_2_cpu1_pid=`top -n 1 -H -p ${vm_2_pid} | grep "CPU 1" | awk '{print $1}'`
 vm_2_cpu2_pid=`top -n 1 -H -p ${vm_2_pid} | grep "CPU 2" | awk '{print $1}'`
 vm_2_cpu3_pid=`top -n 1 -H -p ${vm_2_pid} | grep "CPU 3" | awk '{print $1}'`
+echo vm_2_cpu0_pid is ${vm_2_cpu0_pid}
+echo vm_2_cpu1_pid is ${vm_2_cpu1_pid}
+echo vm_2_cpu2_pid is ${vm_2_cpu2_pid}
+echo vm_2_cpu3_pid is ${vm_2_cpu3_pid}
 taskset -pc -a ${cpu_vm1_core0} ${vm_2_pid}
 taskset -pc ${cpu_vm1_core0} ${vm_2_cpu0_pid}
 taskset -pc ${cpu_vm1_core1} ${vm_2_cpu1_pid}
