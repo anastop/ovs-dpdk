22b24e3aaa462629b7efcd44fecf5bb5166a68f3
Brian Eiler <brianeiler@gmail.com>
2019-04-29 12:36:48 -0400

    Update set-vm-cpus.sh

--- debug/set-vm-cpus.sh
+++ debug/set-vm-cpus.sh
@@ -9,7 +9,7 @@ qemu-affinity -k 2:${cpu_vm1_core2} 3:${cpu_vm1_core3} -- ${vm_1_pid}
 taskset -pc -a ${vm_1_pid}
 echo
 echo "Setting CPU affinity for VPP-VM2."
-vm-2-pid=`ps awx | grep VPP-VM2 | grep -v grep | awk '{print $1}'`
+vm_2_pid=`ps awx | grep VPP-VM2 | grep -v grep | awk '{print $1}'`
 taskset -pc -a ${cpu_vm2_core0},${cpu_vm2_core1} ${vm_2_pid}
 qemu-affinity -k 2:${cpu_vm2_core2} 3:${cpu_vm2_core3} -- ${vm_2_pid}
 taskset -pc -a ${vm_2_pid}
