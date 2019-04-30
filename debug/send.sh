3b3d7dcbe6ba64279748870f291cb088915b3993
Brian Eiler <brianeiler@gmail.com>
2019-04-29 21:08:09 -0400

    updates

--- debug/send.sh
+++ debug/send.sh
@@ -2,12 +2,9 @@
 source /etc/0-ovs-dpdk-global-variables.sh
 
 source_name=${1}
-target_name=`hostname`-${1}
-target_path="https://storage.googleapis.com/incoming-lab"
-echo
-echo "source name: "${source_name}
-echo "target name: "${target_name}
-echo "target path: "${target_path}
-echo
+target_bucketname=${2}
 
-curl -v --upload-file ${source_name} ${target_path}/${target_name}
+target_filename=`hostname`-${1}
+target_path="https://storage.googleapis.com/${target_bucketname}"
+
+curl -v --upload-file ${source_name} ${target_path}/${target_filename}
