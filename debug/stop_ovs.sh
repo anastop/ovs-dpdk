f680eb6f3ba7ef10577d389b2f88444150cbe8a1
Brian Eiler <brianeiler@gmail.com>
2019-05-02 21:35:58 -0400

    Remove unneeded files

--- debug/stop_ovs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Load the custom global environment variables
-source /etc/0-ovs-dpdk-global-variables.sh
-
-$OVS_DIR/utilities/ovs-appctl -t ovsdb-server exit
-$OVS_DIR/utilities/ovs-appctl -t ovs-vswitchd exit
-
