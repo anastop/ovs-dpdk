96a2d742cac56225d4c24d2f3de056d862328249
Brian Eiler <brianeiler@gmail.com>
2019-05-07 00:36:15 -0400

    Update remote-cold-build.sh

--- build/remote-cold-build.sh
+++ build/remote-cold-build.sh
@@ -75,7 +75,7 @@ else
 	sshpass -p "${rootpass}" ssh-copy-id -f -i ./keys/ivm_id_rsa.pub root@${prepserver}
 	
 	scp ./pre-scripts/*.sh root@${prepserver}:~
-	ssh root@<hostname> './1-kernel_upgrade.sh' > /root/install_${prepserver}_phase_1.log 2>&1
+	ssh root@${prepserver} '/root/1-kernel_upgrade.sh' > /root/install_${prepserver}_phase_1.log 2>&1
 	
 	# Cleanup
 	# 	if [ -f ./prep_unlock.sh ]; then
